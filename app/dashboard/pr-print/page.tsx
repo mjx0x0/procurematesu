8e504325ad8ed68cd40a40eb505c2d599b47ca60
mariel villena <villenamarieljoyce@gmail.com>
2026-09-05 21:45:17 +0800

    fix: use secure PR print approver wrapper

--- app/dashboard/pr-print/page.tsx
+++ app/dashboard/pr-print/page.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
-import PRPrintContent from "./PRPrintContent";
+import SecurePRPrintContent from "./SecurePRPrintContent";
 
 export default function Page() {
   return (
     <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
-      <PRPrintContent />
+      <SecurePRPrintContent />
     </Suspense>
   );
 }
